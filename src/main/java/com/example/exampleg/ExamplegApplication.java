6fc78fec00a90ac82407273d39105c1b110bb521
Jongsus <pjs0102303@gmail.com>
2021-06-23 19:18:19 +0900

    Initial commit

---
+++ src/main/java/com/example/exampleg/ExamplegApplication.java
@@ -0,0 +1,13 @@
+package com.example.exampleg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exampleg/ExamplegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exampleg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
